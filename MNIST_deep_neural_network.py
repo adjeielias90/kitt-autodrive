5676e336163fabe020feb9d59f9ebecb2f150d45
adjeielias90 <adjeielias90@gmail.com>
2019-12-18 13:22:59 +0000

    Add network assertions and number of classes

--- MNIST_deep_neural_network.py
+++ MNIST_deep_neural_network.py
@@ -11,3 +11,16 @@ import random
 
 np.random.seed(0)
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape[0])
+
+assert(X_train.shape[0] == y_train.shape[0]), "The number of images is not equal to the number of labels."
+assert(X_test.shape[0] == y_test.shape[0]), "The number of images is not equal to the number of labels."
+assert(X_train.shape[1:] == (28,28)), "The dimensions of the images are not 28x28"
+assert(X_test.shape[1:] == (28,28)), "The dimensions of the images are not 28x28"
+num_of_samples = []
+
+cols = 5
+num_classes = 10
