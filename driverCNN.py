9f2d4f45beb247bb23973c159ec4af92d8ab9b11
adjeielias90 <adjeielias90@gmail.com>
2020-03-13 19:57:54 +0000

    Split data into test and validation sets

--- driverCNN.py
+++ driverCNN.py
@@ -18,7 +18,7 @@ import random
 datadir = './training_data'
 columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
 data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names = columns)
-pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', None)
 data.head()
 
 # Return path of image only
@@ -33,21 +33,67 @@ data.head()
 # Plot steering angles on histogram to visualize data
 # We aim to achive a centered distrubution
 num_bins = 25
-samples_per_bin = 400
+samples_per_bin = 200
 hist, bins = np.histogram(data['steering'], num_bins)
 center = (bins[:-1]+ bins[1:]) * 0.5
 plt.bar(center, hist, width=0.05)
 plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
 plt.show()
 
-# Shuffle data
+# Shuffle and re-arrange data
 print('total data:', len(data))
 remove_list = []
 for j in range(num_bins):
   list_ = []
   for i in range(len(data['steering'])):
+    # all steering angles of a specific bin
     if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j+1]:
       list_.append(i)
   list_ = shuffle(list_)
   list_ = list_[samples_per_bin:]
   remove_list.extend(list_)
+
+# Splice data that may affect our model accuracy
+# Cutting out bins who's frequency exceeds the 200 marks
+print('removed:', len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('remaining:', len(data))
+
+# Plotting another histogram with cleaned data
+hist, _ = np.histogram(data['steering'], (num_bins))
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+# Nice one
+plt.show()
+
+# Import driving data
+# Print data from index[1]
+print(data.iloc[1])
+
+# Loading data into test and train data
+def load_img_steering(datadir, df):
+  image_path = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    # set center, right, left angles
+    center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+    image_path.append(os.path.join(datadir, center.strip()))
+    steering.append(float(indexed_data[3]))
+    # left image append
+    image_path.append(os.path.join(datadir,left.strip()))
+    steering.append(float(indexed_data[3])+0.15)
+    # right image append
+    image_path.append(os.path.join(datadir,right.strip()))
+    steering.append(float(indexed_data[3])-0.15)
+  # Add image_paths and steerings as numpy arrays
+  image_paths = np.asarray(image_path)
+  steerings = np.asarray(steering)
+  # Finally, return paths
+  return image_paths, steerings
+
+image_paths, steerings = load_img_steering(datadir + '/IMG', data)
+
+# Split data into test and training sets
+X_train, X_valid, y_train, y_valid = train_test_split(image_paths, steerings, test_size=0.2, random_state=6)
+print('Training Samples: {}\nValid Samples: {}'.format(len(X_train), len(X_valid)))
