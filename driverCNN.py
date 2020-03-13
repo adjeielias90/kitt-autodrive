75be76a73927f2d49076713abc56a0b105c615ce
adjeielias90 <adjeielias90@gmail.com>
2020-03-13 17:06:28 +0000

    Visualize steering angles

--- driverCNN.py
+++ driverCNN.py
@@ -14,12 +14,14 @@ import pandas as pd
 import ntpath
 import random
 
+# Read CSV with pandas
 datadir = './training_data'
 columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
 data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names = columns)
 pd.set_option('display.max_colwidth', -1)
 data.head()
 
+# Return path of image only
 def path_leaf(path):
   head, tail = ntpath.split(path)
   return tail
@@ -27,3 +29,12 @@ data['center'] = data['center'].apply(path_leaf)
 data['left'] = data['left'].apply(path_leaf)
 data['right'] = data['right'].apply(path_leaf)
 data.head()
+
+# Plot steering angles on histogram to visualize data
+# We aim to achive a centered distrubution
+num_bins = 25
+samples_per_bin = 400
+hist, bins = np.histogram(data['steering'], num_bins)
+center = (bins[:-1]+ bins[1:]) * 0.5
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
