b0759768db06de7487fab4a4dbde088d59a9390f
adjeielias90 <adjeielias90@gmail.com>
2020-03-16 11:50:56 +0000

    More changes

--- behavioral_cloning/driver.py
+++ behavioral_cloning/driver.py
@@ -1,16 +1,26 @@
 import socketio
 import eventlet
 from flask import Flask
-
-
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
 
 sio = socketio.Server()
 app = Flask(__name__) #'__main__'
 
+@sio.on('telemetry')
+def telemetry(sid, data):
+  image = Image.open(BytesIO(base64.b64encode(data['image'])))
+
+
+
+
 @sio.on('connect')
 def connect(sid, environ):
   print('connected')
-  send_control(0, 1)
+  send_control(0, 0)
+
 
 
 def send_control(steering_angle, throttle):
@@ -20,5 +30,6 @@ def send_control(steering_angle, throttle):
   })
 
 if __name__ == '__main__':
+  model = load_model('kitt.h5')
   app = socketio.Middleware(sio, app)
   eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
