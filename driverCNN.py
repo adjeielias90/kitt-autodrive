6e9c9f0e8eb59bab655e1e350feda3492959a7bf
adjeielias90 <adjeielias90@gmail.com>
2020-03-16 09:40:44 +0000

    Add Nvidia model

--- driverCNN.py
+++ driverCNN.py
@@ -107,3 +107,34 @@ axes[0].set_title('Training set')
 # Plot steering angles for validation data
 axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
 axes[1].set_title('Validation set')
+
+
+def nvidia_model():
+  model = Sequential()
+  model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='elu'))
+  model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='elu'))
+  model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='elu'))
+  model.add(Convolution2D(64, 3, 3, activation='elu'))
+
+  model.add(Convolution2D(64, 3, 3, activation='elu'))
+#   model.add(Dropout(0.5))
+
+
+  model.add(Flatten())
+
+  model.add(Dense(100, activation = 'elu'))
+#   model.add(Dropout(0.5))
+
+  model.add(Dense(50, activation = 'elu'))
+#   model.add(Dropout(0.5))
+
+  model.add(Dense(10, activation = 'elu'))
+#   model.add(Dropout(0.5))
+
+  model.add(Dense(1))
+
+  optimizer = Adam(lr=1e-3)
+  model.compile(loss='mse', optimizer=optimizer)
+  return model
+model = nvidia_model()
+print(model.summary())
